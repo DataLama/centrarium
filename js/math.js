d9e4af84f6edc2a0d56429fc4af92e99d089665d
DataLama <38907104+DataLama@users.noreply.github.com>
2018-07-30 19:53:15 +0900

    Update math.js

--- js/math.js
+++ js/math.js
@@ -13,8 +13,6 @@
   head.appendChild(script);
 })();
 
-
-
 (function () {
   var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js';
   var oldMathJax = 'cdn.mathjax.org/mathjax/latest/MathJax.js';
@@ -68,6 +66,3 @@
     }
   }
 })();
-
-
-
